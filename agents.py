44cdaf0845128fc2472973190621695c6a9fe098
enriquemaestro <emaestro@ucm.es>
2021-05-23 19:07:46 +0200

    Agente analizador

--- agents.py
+++ agents.py
@@ -30,6 +30,8 @@ from sklearn.preprocessing import LabelEncoder
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 
+import nltk
+
 #******************************************************************************************************************************************
 #******************************************************************************************************************************************
 #******************************************************************************************************************************************
@@ -46,9 +48,13 @@ class ChatBotAgent(Agent):
 
         # Texto a responder para clasificar.
         self.answerForClassification = "give me the new"
+        self.answerForAnalyze = "choose the new"
 
         # Texto a mostrar por defecto cuando no se entiende la entrada del usuario.
         self.defaultAnswer = "I'm sorry, but I don't understand."
+
+        # True == Classify, False == Analyze
+        self.classifyOrAnalyze = None
 
     @staticmethod
     def setUserText(text):
@@ -104,7 +110,12 @@ class ChatBotAgent(Agent):
                     ['classify new', self.agent.answerForClassification],
                     ['classify that', self.agent.answerForClassification],
                     ['classify this', self.agent.answerForClassification],
-                    ['i want you to classify this new', self.agent.answerForClassification]
+                    ['i want you to classify this new', self.agent.answerForClassification],
+                    ['analyze', self.agent.answerForAnalyze],
+                    ['analyze new', self.agent.answerForAnalyze],
+                    ['analyze that', self.agent.answerForAnalyze],
+                    ['analyze this', self.agent.answerForAnalyze],
+                    ['i want you to analyze this new', self.agent.answerForAnalyze]
                 )
 
                 # Objeto para entrenar al bot con las conversaciones definidas.
@@ -162,9 +173,15 @@ class ChatBotAgent(Agent):
 
             # Pasamos a clasificar o ciclamos en el estado.
             if text == self.agent.answerForClassification:
+                self.agent.classifyOrAnalyze = True
+                self.set_next_state("SEND_STATE")
+            elif text == self.agent.answerForAnalyze:
+                self.agent.classifyOrAnalyze = False
                 self.set_next_state("SEND_STATE")
             else:
                 self.set_next_state("INPUT_STATE")
+            
+            self.agent.classifyOrAnalyze = None
 
     class sendState(State):
 
@@ -176,7 +193,7 @@ class ChatBotAgent(Agent):
                 pass
 
             # Envía el mensaje.
-            await self.send(msg=Message(to="dasi2@blabber.im", body=ChatBotAgent.getUserText()))
+            await self.send(msg=Message(to="dasi2@blabber.im" if self.agent.classifyOrAnalyze else "dasi3@blabber.im", body=ChatBotAgent.getUserText()))
 
             # Si no se introduce un poco de retardo, el envío podría no completarse.
             await asyncio.sleep(0.2)
@@ -420,3 +437,111 @@ class ClassifierAgent(Agent):
 #******************************************************************************************************************************************
 #******************************************************************************************************************************************
 #******************************************************************************************************************************************
+
+class AnalyzerAgent(Agent):
+
+    def __init__(self, *args, **kwargs):
+        
+        # Llamada a la super (Agent).
+        super().__init__(*args, **kwargs)
+
+    # Esta clase interna sirve para definir el comportamiento del agente.
+    class FsmBehaviour(FSMBehaviour):
+        pass
+
+    class receiveState(State):
+
+        # Este método se llama después de ejecutarse on_start().
+        async def run(self):
+
+            # Espera como mucho N segundos para recibir algún mensaje.
+            msg = await self.receive(timeout=3600)
+            
+            # msg es un objeto o bien Message o bien None.
+            if msg:
+                    
+                # Comprobamos que la noticia esté en la carpeta.                
+                if exists(join("news", msg.body)):
+
+                    # Aquí volcaremos el contenido del fichero.
+                    fileContent = None
+
+                    # Leemos el contenido del fichero.
+                    with open(join("news", msg.body), 'r') as file:
+
+                        # Obtenemos el contenido del fichero.
+                        fileContent = file.read()
+
+                    sentences = nltk.sent_tokenize(fileContent)
+                    tokenized_sentences = [nltk.word_tokenize(sentence) for sentence in sentences]
+                    tagged_sentences = [nltk.pos_tag(sentence) for sentence in tokenized_sentences]
+                    chunked_sentences = nltk.ne_chunk_sents(tagged_sentences, binary=True)
+
+                    def extract_entity_names(t):
+                        entity_names = []
+
+                        if hasattr(t, 'label') and t.label:
+                            if t.label() == 'NE':
+                                entity_names.append(' '.join([child[0] for child in t]))
+                            else:
+                                for child in t:
+                                    entity_names.extend(extract_entity_names(child))
+
+                        return entity_names
+
+
+                    entity_names = []
+                    for tree in chunked_sentences:
+                        # Print results per sentence
+                        # print extract_entity_names(tree)
+
+                        entity_names.extend(extract_entity_names(tree))
+
+                    # Print all entity names
+                    #print(entity_names)
+
+                    # Print unique entity names
+                    #print(set(entity_names))
+
+                    self.agent.lastAnalyze = set(entity_names)
+
+                    #print(sorted(names.items(), key=lambda x:x[1], reverse=True))
+
+                else:
+
+                    # No se ha encontrado la noticia.
+                    self.agent.lastAnalyze = "The notice wasn't found in 'news' folder"
+
+            # Una vez se ha clasificado la noticia pasamos al estado de envío para informar al agente ChatBot.
+            self.set_next_state("SEND_STATE")
+
+    class sendState(State):
+
+        # Este método se llama después de ejecutarse on_start().
+        async def run(self):
+            
+            # Envía el mensaje.
+            await self.send(msg=Message(to="dasi1@blabber.im", body=self.agent.lastAnalyze))
+
+            # Si no se introduce un poco de retardo, el envío podría no completarse.
+            await asyncio.sleep(0.2)
+
+            # Pasamos al estado de escucha para que el agente de clasificación nos pueda devolver el tipo de noticia.
+            self.set_next_state("RECEIVE_STATE")
+    
+    # Este método se llama cuando se inicializa el agente.
+    async def setup(self):
+
+        # Declaramos el comportamiento compuesto.
+        fsm = self.FsmBehaviour()
+        
+        # Declaramos los subcomportamientos.
+        fsm.add_state(name="RECEIVE_STATE", state=self.receiveState(), initial=True)
+        fsm.add_state(name="SEND_STATE", state=self.sendState())
+
+        # Declaramos las posibles transiciones entre estados.
+        fsm.add_transition(source="RECEIVE_STATE", dest="SEND_STATE")
+        fsm.add_transition(source="SEND_STATE", dest="RECEIVE_STATE")
+
+        # Encolamos el siguiente comportamiento.
+        self.add_behaviour(behaviour=fsm, template=Template(to="dasi3@blabber.im"))
