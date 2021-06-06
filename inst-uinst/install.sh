fb951d164546b8a8a5ae33f60e92ea14589dffa3
ManuelGMS <manguemon@gmail.com>
2021-06-06 05:21:17 +0200

    Simplificando la isntalacion

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -7,22 +7,9 @@ sudo apt-get -y install python3-pip
 # Librerias de python.
 python3 -m pip install -U pip
 python3 -m pip install -U nltk
-python3 -m pip install -U pyyaml
 python3 -m pip install -U chatterbot
 python3 -m pip install -U chatterbot_corpus
 python3 -m pip install -U spade
 python3 -m pip install -U pandas
 python3 -m pip install -U sklearn
 python3 -m pip install -U googlenews
-
-# Librerias de python.
-#PATH="$HOME/.local/bin:$PATH"
-#pip3 install -U pip
-#pip3 install -U nltk
-#pip3 install -U pyyaml
-#pip3 install -U chatterbot
-#pip3 install -U chatterbot_corpus
-#pip3 install -U spade
-#pip3 install -U pandas
-#pip3 install -U sklearn
-#pip3 install -U googlenews
--- launcher.sh
+++ launcher.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-#PATH="$HOME/.local/bin:$PATH"
 export NLTK_DATA=$PWD/nltk_data
-python3 main.py #1> /dev/null 2> /dev/null
+python3 main.py 1> /dev/null 2> /dev/null
