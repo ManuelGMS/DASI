8b3e5af5eca1aa21cbc7ab952a5e3e32ee03452a
ManuelGMS <manguemon@gmail.com>
2021-06-06 05:05:22 +0200

    Instalacion simplificada

--- inst-uinst/uninstall.sh
+++ inst-uinst/uninstall.sh
@@ -25,17 +25,5 @@ if [ "$input" = "y" ]; then
 
 fi
 
-# venv
-
-echo -e "\nWould you like to delete venv package of your system? (y/n): "
-read -n 1 input
-
-if [ "$input" = "y" ]; then
-
-	sudo apt-get -y install python3-venv
-	
-fi
-
-
 # end
 echo -e "\n"
--- launcher.sh
+++ launcher.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+#PATH="$HOME/.local/bin:$PATH"
 export NLTK_DATA=$PWD/nltk_data
-python3 main.py 1> /dev/null 2> /dev/null
+python3 main.py #1> /dev/null 2> /dev/null
