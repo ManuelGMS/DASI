c3c1bf6d349074c601e6e952797d9300da82dd86
ManuelGMS <manguemon@gmail.com>
2021-06-06 04:24:55 +0200

    Instalacion simple

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
+
+# Librerias del sistema.
 sudo apt-get -y install python3-tk
 sudo apt-get -y install python3-pip
 sudo apt-get -y install python3-venv
+
+# Librerias de python.
+pip3 install -U pip
+pip3 install -U nltk
+pip3 install -U pyyaml
+pip3 install -U chatterbot
+pip3 install -U chatterbot_corpus
+pip3 install -U spade
+pip3 install -U pandas
+pip3 install -U sklearn
+pip3 install -U googlenews
--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,3 @@
 #!/bin/bash
-source dasi-venv/bin/activate
 export NLTK_DATA=$PWD/nltk_data
 python3 main.py 1> /dev/null 2> /dev/null
-deactivate
