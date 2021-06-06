2008eb756f4073074ef5bc1869d40f79cf4dfae7
Manuel Guerrero Moñús <manguemon@gmail.com>
2021-06-07 01:12:34 +0200

    Correccion en el instalador

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -14,21 +14,21 @@ python3 -m pip install -U pip 1> /dev/null 2> /dev/null
 echo "Instalado: 3/12"
 
 # Instalamos las librerias de python.
-python3 -m pip install -U nltk 1> /dev/null 2> /dev/null
+python3 -m pip install nltk 1> /dev/null 2> /dev/null
 echo "Instalado: 4/12"
-python3 -m pip install -U spacy 1> /dev/null 2> /dev/null
+python3 -m pip install spacy 1> /dev/null 2> /dev/null
 echo "Instalado: 5/12"
-python3 -m pip install -U chatterbot 1> /dev/null 2> /dev/null
+python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
 echo "Instalado: 6/12"
-python3 -m pip install -U chatterbot_corpus 1> /dev/null 2> /dev/null
+python3 -m pip install chatterbot_corpus 1> /dev/null 2> /dev/null
 echo "Instalado: 7/12"
-python3 -m pip install -U spade 1> /dev/null 2> /dev/null
+python3 -m pip install spade 1> /dev/null 2> /dev/null
 echo "Instalado: 8/12"
-python3 -m pip install -U pandas 1> /dev/null 2> /dev/null
+python3 -m pip install pandas 1> /dev/null 2> /dev/null
 echo "Instalado: 9/12"
-python3 -m pip install -U sklearn 1> /dev/null 2> /dev/null
+python3 -m pip install sklearn 1> /dev/null 2> /dev/null
 echo "Instalado: 10/12"
-python3 -m pip install -U googlenews 1> /dev/null 2> /dev/null
+python3 -m pip install googlenews 1> /dev/null 2> /dev/null
 echo "Instalado: 11/12"
-python3 -m pip install -U chatterbot 1> /dev/null 2> /dev/null
+python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
 echo "Instalado: 12/12"
