858c06bf8398b90236aba2044a214200ec49a222
enriquemaestro <emaestro@ucm.es>
2021-06-06 19:04:51 +0200

    Agregada libreria spacy a la instalacion

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -7,6 +7,7 @@ sudo apt-get -y install python3-pip
 # Librerias de python.
 python3 -m pip install -U pip
 python3 -m pip install -U nltk
+python3 -m pip install -U spacy
 python3 -m pip install -U chatterbot
 python3 -m pip install -U chatterbot_corpus
 python3 -m pip install -U spade
@@ -17,6 +18,7 @@ python3 -m pip install -U googlenews
 # Librerias de python.
 pip3 install -U pip
 pip3 install -U nltk
+pip3 install -U spacy
 pip3 install -U chatterbot
 pip3 install -U chatterbot_corpus
 pip3 install -U spade
