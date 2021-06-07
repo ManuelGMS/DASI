b244ba9ad270236dbb28611d2877239fbe85243a
Manuel Guerrero Moñús <manguemon@gmail.com>
2021-06-07 09:21:46 +0200

    Cambio en el isntalador

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -5,30 +5,30 @@ PATH="$PATH:$HOME/.local/bin"
 
 # Instalamos las librerias del sistema.
 sudo apt-get -y install python3-tk 1> /dev/null 2> /dev/null
-echo "Instalado: 1/12"
+echo "Instalado: 1/11"
 sudo apt-get -y install python3-pip 1> /dev/null 2> /dev/null
-echo "Instalado: 2/12"
+echo "Instalado: 2/11"
 
 # Actualizamos el instalador pip.
 python3 -m pip install -U pip 1> /dev/null 2> /dev/null
-echo "Instalado: 3/12"
+echo "Instalado: 3/11"
+
+chmod -R 777 /usr/lib/python3*
 
 # Instalamos las librerias de python.
 python3 -m pip install nltk 1> /dev/null 2> /dev/null
-echo "Instalado: 4/12"
+echo "Instalado: 4/11"
 python3 -m pip install spacy 1> /dev/null 2> /dev/null
-echo "Instalado: 5/12"
-python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
-echo "Instalado: 6/12"
+echo "Instalado: 5/11"
 python3 -m pip install chatterbot_corpus 1> /dev/null 2> /dev/null
-echo "Instalado: 7/12"
+echo "Instalado: 6/11"
 python3 -m pip install spade 1> /dev/null 2> /dev/null
-echo "Instalado: 8/12"
+echo "Instalado: 7/11"
 python3 -m pip install pandas 1> /dev/null 2> /dev/null
-echo "Instalado: 9/12"
+echo "Instalado: 8/11"
 python3 -m pip install sklearn 1> /dev/null 2> /dev/null
-echo "Instalado: 10/12"
+echo "Instalado: 9/11"
 python3 -m pip install googlenews 1> /dev/null 2> /dev/null
-echo "Instalado: 11/12"
+echo "Instalado: 10/11"
 python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
-echo "Instalado: 12/12"
+echo "Instalado: 11/11"
