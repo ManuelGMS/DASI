d6f0b6308cf53287018189996739ab2e0f630e11
Manuel Guerrero Moñús <manguemon@gmail.com>
2021-06-07 17:45:02 +0200

    Ajustes en el instalador

--- inst-uinst/install.sh
+++ inst-uinst/install.sh
@@ -4,31 +4,40 @@
 PATH="$PATH:$HOME/.local/bin"
 
 # Instalamos las librerias del sistema.
+echo "Instalando: python3-tk"
 sudo apt-get -y install python3-tk 1> /dev/null 2> /dev/null
-echo "Instalado: 1/12"
+echo "Se ha Instalado: python3-tk"
+echo "Instalando: python3-pip"
 sudo apt-get -y install python3-pip 1> /dev/null 2> /dev/null
-echo "Instalado: 2/12"
+echo "Se ha Instalado: python3-pip"
 
 # Actualizamos el instalador pip.
+echo "Actualizando: pip"
 python3 -m pip install -U pip 1> /dev/null 2> /dev/null
-echo "Instalado: 3/12"
+echo "Se ha actualizado: pip"
 
 # Instalamos las librerias de python.
-python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
-echo "Instalado: 4/12"
+echo "Instalando: nltk"
 python3 -m pip install nltk 1> /dev/null 2> /dev/null
-echo "Instalado: 5/12"
+echo "Se ha Instalado: nltk"
+echo "Instalando: spacy"
 python3 -m pip install spacy 1> /dev/null 2> /dev/null
-echo "Instalado: 6/12"
+echo "Se ha Instalado: spacy"
+echo "Instalando: chatterbot_corpus"
 python3 -m pip install chatterbot_corpus 1> /dev/null 2> /dev/null
-echo "Instalado: 7/12"
+echo "Se ha Instalado: chatterbot_corpus"
+echo "Instalando: spade"
 python3 -m pip install spade 1> /dev/null 2> /dev/null
-echo "Instalado: 8/12"
+echo "Se ha Instalado: spade"
+echo "Instalando: pandas"
 python3 -m pip install pandas 1> /dev/null 2> /dev/null
-echo "Instalado: 9/12"
+echo "Se ha Instalado: pandas"
+echo "Instalando: sklearn"
 python3 -m pip install sklearn 1> /dev/null 2> /dev/null
-echo "Instalado: 10/12"
+echo "Se ha Instalado: sklearn"
+echo "Instalando: googlenews"
 python3 -m pip install googlenews 1> /dev/null 2> /dev/null
-echo "Instalado: 11/12"
+echo "Se ha Instalado: googlenews"
+echo "Instalando: chatterbot"
 python3 -m pip install chatterbot 1> /dev/null 2> /dev/null
-echo "Instalado: 12/12"
+echo "Se ha Instalado: chatterbot"
